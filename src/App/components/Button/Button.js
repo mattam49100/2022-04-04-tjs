719c21a43b22b1ea463edee769ed51a2c1a1a176
mattam49100 <matthieu.boivin@live.fr>
2022-04-04 15:40:35 +0200

    observation des changement de valeur

--- src/App/components/Button/Button.js
+++ src/App/components/Button/Button.js
@@ -1,13 +1,28 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import style from "./Button.module.css";
 import PropTypes from "prop-types";
 
 function Button(props) {
-  console.log(props);
   const [clicked, setClicked] = useState(false);
+  //observation des changement / montage de la valeur
+  useEffect(() => {
+    if (clicked) {
+      setTimeout(() => {
+        setClicked(false);
+      }, 500);
+    }
+  }, [clicked]);
+
+  // cmp dit mount
+  useEffect(() => {
+      console.log('le composant est monté')
+  }, []);
+
+  console.log(props);
   return (
     <button
       onClick={(evt) => {
+          // modif async d une valeur d'état
         setClicked(true);
         setTimeout(() => {
           setClicked(false);
