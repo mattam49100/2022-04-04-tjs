f69f727ae7906a293280a4e7d85321437d7a25fc
mattam49100 <matthieu.boivin@live.fr>
2022-04-04 16:48:28 +0200

    COMPOSANTE

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,39 +1,35 @@
-import React, { useState } from "react";
-import "./App.css";
+import React, { Component } from "react";
+import style from "./App.module.css";
 import Button from "./components/Button/Button";
 
-function App() {
-  const [state, setstate] = useState(0)
- // let nbClicked=0;
-  return (
-    <div className="App">
-      hello world : {state}
-      <Button
-        bgcolor="red"
-        lorsqueLeButtonEstClicked={(arg: any) => {
-          setstate(state+1)
-         // console.log(arg);
-          console.log(state); 
-        }}
-      >
-        hello {state}
-      </Button>
-      <Button
-        bgcolor="blue"
-        color="white"
-        lorsqueLeButtonEstClicked={(arg: any) => {
-          setstate(state+1)
-          console.log(arg); 
-        }}
-      >
-        <div>hello avec div</div>
-      </Button>
-      <Button style={{border:'1px solid #000'}}>
-        <div>div 1</div>
-        <div>div 2</div> 
-      </Button>
-    </div>
-  );
+interface I_AppProps {
+  AppName?: string;
+}
+interface I_AppState {
+  counter: number;
+  uneValue: string;
 }
 
+class App extends Component<I_AppProps, I_AppState> {
+  constructor(props: I_AppProps) {
+    super(props);
+    this.state = { counter: 0, uneValue: "Hello" };
+  }
+
+  render(): React.ReactNode {
+    return (
+      <div className={style.App}>
+        value du compteur : {this.state.counter}
+        <br />
+        <Button
+          lorsqueLeButtonEstClicked={() => {
+            this.setState({ counter: this.state.counter + 1 });
+          }}
+        >
+          {this.state.uneValue}
+        </Button>
+      </div>
+    );
+  }
+}
 export default App;
