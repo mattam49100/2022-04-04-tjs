bf9bd87892f9ccb692ce95bac68dbb25f55e9fd0
mattam49100 <matthieu.boivin@live.fr>
2022-04-04 14:33:52 +0200

    defaultPorps + proptypes

--- src/App/components/Button/Button.js
+++ src/App/components/Button/Button.js
@@ -1,10 +1,30 @@
 import React from 'react'; 
 import style from './Button.module.css'
+import PropTypes from 'prop-types';
 
 function Button(props) { 
     console.log(props);
     return (
-    <button className="Button" style={{backgroundColor: props.bgcolor}}>{props.text}</button>
+    <button 
+    onClick={(evt) => {
+        props.lorsqueLeButtonEstClicked("Hello");
+    }}
+    className={style.Button}
+    style={{backgroundColor: props.bgcolor}}
+    >
+    {props.text}
+    </button>
     );
+}
+Button.propTypes= {
+    text: PropTypes.string.isRequired,
+    lorsqueLeButtonEstClicked : PropTypes.func.isRequired,
+    bgcolor: PropTypes.string
+}
+
+Button.defaultProps={
+    lorsqueLeButtonEstClicked:()=>{
+        console.error('fonction du button undefined')
+    }
 }
 export default Button; 
